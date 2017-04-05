6b93a4ec3c910fe0b8d96104c1e0881e2a494060
ajb871 <ajb871@nyu.edu>
2017-04-05 11:34:38 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,16 +1,31 @@
 var emotion_value = 0;
-var emotion = document.forms[0];
+
+//var emotion = document.forms[0];
 
 
+var submitButton = document.getElementById("submit_button");
 
-
-
-
-
-function addValues(){
+function getValues(){
+	console.log('getValues executed');
 	var checkedValue;
 	var answerElements = document.getElementsByClassName('option');
+	for(var i=0; i < answerElements.length; i ++){
+		if (answerElements[i].checked){
+			checkedValue += answerElements[i].value;
+		}
+	}
+	console.log(checkedValue);
 	console.log(answerElements);
+	emotion_value += checkedValue;
+}
+
+//submitButton.addEventListener('click', getValues());
+
+/*
+function addValues(){
+	var checkedValue;
+	//var answerElements = document.getElementsByClassName('option');
+	//console.log(answerElements);
 	for(var i=0; i < answerElements.length; i ++){
 		if (answerElements[i].checked){
 			checkedValue += answerElements[i].value;
@@ -19,8 +34,10 @@ function addValues(){
 	emotion_value += checkedValue;
 	console.log(emotion_value);
 }
+*/
 
-function submitHandler(event){
+
+/*function submitHandler(event){
 	event.preventDefault();
 	console.log(emotion_value);
-}
+}*/
